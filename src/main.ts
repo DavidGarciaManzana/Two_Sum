a89d779c745c8cdeca6e0151ea00961257bcfddd
David Garcia <Davidgarciama@outlook.com>
2023-02-15 14:56:25 -0600

    Try to emulate a hash table using an object

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,25 @@
-// let twoSum = (nums: number[], target: number): number[] => {
-//     let result: number[] = [];
-//     nums.forEach((number1: number, index1: number) => {
-//         nums.slice(index1 + 1).forEach((number2: number, index2: number) => {
-//             if (number1 == number2) {
-//                 if (number1 + number2 == target) {
-//                     result.push(index1, nums.indexOf(number1, index1 + 1))
-//                 }
-//             } else {
-//                 if (number1 + number2 == target) {
-//                     result.push(index1, nums.indexOf(number2))
-//                 }
-//             }
-//         })
-//     })
-//     return result
-// }
-
+interface MyObject {
+    [key: number]: any;
+    // otras propiedades y métodos aquí
+  }
 
 let twoSum = (nums: number[], target: number): number[] => {
+    // console.log("aaa")
+    let myObj: MyObject = {};
     for (let i = 0; i < nums.length; i++) {
-        let newArray = nums.slice();
-        newArray.splice(i, 1)
-        if (newArray.includes(target - nums[i])) {
-            console.log(newArray)
-            if (target - nums[i] == nums[i]) {
-                return [i, nums.indexOf(target - nums[i], i + 1)]
-            } else {
-                return [i, nums.indexOf(target - nums[i])]
-            }
-        }
+        // console.log("bbb")
+        myObj[nums[i]] = i
+        console.log(myObj)
+        // let newArray = nums.slice();
+        // newArray.splice(i, 1)
+        // if (newArray.includes(target - nums[i])) {
+        //     // console.log(newArray)
+        //     if (target - nums[i] == nums[i]) {
+        //         return [i, nums.indexOf(target - nums[i], i + 1)]
+        //     } else {
+        //         return [i, nums.indexOf(target - nums[i])]
+        //     }
+        // }
     }
     return []
 }
@@ -36,4 +27,4 @@ let twoSum = (nums: number[], target: number): number[] => {
 // Testing
 // console.log(twoSum([2, 7, 11, 15], 9)) //[0,1]
 // console.log(twoSum([3, 2, 4], 6)) //[1,2]
-console.log(twoSum([3, 3], 6)) //[0,1]
+twoSum([3, 3], 6) //[0,1]
