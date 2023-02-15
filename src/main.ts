c90570d7aa926b6553d81a22f41effeab0435106
David Garcia <Davidgarciama@outlook.com>
2023-02-15 13:37:55 -0600

    Firs attempt to try to get rid of the second for

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,33 @@
+// let twoSum = (nums: number[], target: number): number[] => {
+//     let result: number[] = [];
+//     nums.forEach((number1: number, index1: number) => {
+//         nums.slice(index1 + 1).forEach((number2: number, index2: number) => {
+//             if (number1 == number2) {
+//                 if (number1 + number2 == target) {
+//                     result.push(index1, nums.indexOf(number1, index1 + 1))
+//                 }
+//             } else {
+//                 if (number1 + number2 == target) {
+//                     result.push(index1, nums.indexOf(number2))
+//                 }
+//             }
+//         })
+//     })
+//     return result
+// }
+
+
 let twoSum = (nums: number[], target: number): number[] => {
-    let result: number[] = [];
-    nums.forEach((number1: number, index1: number) => {
-        nums.slice(index1 + 1).forEach((number2: number, index2: number) => {
-            if (number1 == number2) {
-                if (number1 + number2 == target) {
-                    result.push(index1, nums.indexOf(number1, index1 + 1))
-                }
-            } else {
-                if (number1 + number2 == target) {
-                    result.push(index1, nums.indexOf(number2))
-                }
-            }
-        })
+    // let newArray;
+    nums.forEach((number, index) => {
+        let newArray = nums.slice();
+        newArray.splice(index, 1)
     })
-    return result
+    return []
+
 }
 
 // Testing
-// console.log(twoSum([2,7,11,15],9)) //[0,1]
-// console.log(twoSum([3,2,4],6)) //[1,2]
-// console.log(twoSum([3,3],6)) //[0,1]
+console.log(twoSum([2, 7, 11, 15], 9)) //[0,1]
+// console.log(twoSum([3, 2, 4], 6)) //[1,2]
+// console.log(twoSum([3, 3], 6)) //[0,1]
